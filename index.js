d7a507075f77bed1cdde423ef7c02682746d1a03
Douglas Soares Silva <44717994+neodouglas@users.noreply.github.com>
2023-01-06 17:38:58 -0300

    -- Reformulado & Adição do getServerStatusAll
    -- Reformulação completa do código visando eficiência com o estilo de Clean Code.
    -- Criação de "getServerStatusAll" que irá ser uma versão alternative de "getServerStatus" que serve para pegar os status de todos os servidores de uma vez.

--- index.js
+++ index.js
@@ -1,9 +1,89 @@
-const version = require("./src/version.js");
-const player = require("./src/player.js");
-const server = require("./src/server.js");
+const needle = require("needle");
+const core_data = require("./core/core.json");
+
+async function getServerStatus(serverName) {
+  let live = await needle("get", core_data.servers.live);
+  let advance = await needle("get", core_data.servers.advance);
+  let review = await needle("get", core_data.servers.review);
+
+  switch (serverName) {
+    case "live":
+      return { isOn: live.statusCode != 503 };
+    case "advance":
+      return { isOn: advance.statusCode != 503 };
+    case "review":
+      return { isOn: review.statusCode != 503 };
+  }
+}
+
+async function getServerStatusAll() {
+  let live = await needle("get", core_data.servers.live);
+  let advance = await needle("get", core_data.servers.advance);
+  let review = await needle("get", core_data.servers.review);
+
+  return {
+    live: { isOn: live.statusCode != 503 },
+    advance: { isOn: advance.statusCode != 503 },
+    review: { isOn: review.statusCode != 503 },
+  };
+}
+
+async function getVersionInfo() {
+  let response = await needle("get", core_data.version.check);
+
+  switch (response.body.news) {
+    case "true":
+      return {
+        newVersionAvailable: true,
+        currentVersion: response.body.last,
+        newVersion: response.body.version,
+      };
+    case "false":
+      return {
+        newVersionAvailable: false,
+        currentVersion: response.body.last,
+      };
+  }
+}
+
+function format_nick(body) {
+  return {
+    nickname: body.nick_atual,
+    region: body.region,
+    date: body.data,
+    nickHist: body.nicks_anteriores,
+  };
+}
+
+async function getPlayerInfo(playerId) {
+  let nickRequest = await needle("get", core_data.player.nick + playerId);
+
+  switch (nickRequest.statusCode) {
+    case 200:
+      data = format_nick(nickRequest.body);
+
+      return {
+        profile: {
+          nickname: data.nickname,
+        },
+        account: {
+          region: data.region,
+          creationDate: data.date.includes("@") ? null : data.date,
+        },
+        history: {
+          nicknames: data.nickHist,
+        },
+      };
+    case 404 || 500:
+      return {
+        error: "invalid_uid",
+      };
+  }
+}
 
 module.exports = {
-    version,
-    player,
-    server,
-}
+  getServerStatus,
+  getServerStatusAll,
+  getVersionInfo,
+  getPlayerInfo,
+};
